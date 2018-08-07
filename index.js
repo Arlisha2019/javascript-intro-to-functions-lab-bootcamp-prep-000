7addb7aa66b574096dbc8e11bc7e3d126066e8d0
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-07 17:21:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase() === string) {
   return "I can't hear you!"
 } else if(string.toUpperCase() === string) {
-  return "Yes indeed!"
+  return "YES INDEED!"
 } else if(string === "I love you, Grandma.") {
   return "I love you, too."
  }
